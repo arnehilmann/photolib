e507757ff6be383bf2d2882d93c5ba59a77413b2
Arne Hilmann <arne@hilmann.de>
2015-06-23 10:50:33 +0200

    switch to pybuilder

---
+++ build.py
@@ -0,0 +1,28 @@
+from pybuilder.core import use_plugin, init, Author
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+use_plugin("python.flake8")
+use_plugin("python.frosted")
+use_plugin("python.coverage")
+use_plugin("python.distutils")
+
+
+name = "photolib"
+description = """manage your photo library: transfer, rename, clean exif, rotate, prepare for moaic, ..."""
+summary = description
+authors = [Author("Arne Hilmann", "arne.hilmann@gmail.com")]
+url = "https://github.com/arnehilmann/photolib"
+version = "0.1"
+
+default_task = "publish"
+
+
+@init
+def set_properties(project):
+    project.build_depends_on("mock")
+    project.depends_on("docopt")
+
+    project.set_property("flake8_verbose_output", True)
+    project.set_property("flake8_break_build", True)
