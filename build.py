ea7ed823e6723f8514a382bf20c568ebd9d5565e
Arne Hilmann <arne@hilmann.de>
2015-06-24 13:16:28 +0200

    add integrationtest plugin

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ from pybuilder.core import use_plugin, init, Author
 
 use_plugin("python.core")
 use_plugin("python.unittest")
+use_plugin("python.integrationtest")
 use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
 use_plugin("python.frosted")
