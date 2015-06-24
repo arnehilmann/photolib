1af1ae36322915dbad5c66d9985e30baab2c7a58
Arne Hilmann <arne@hilmann.de>
2015-06-24 13:16:43 +0200

    make flake8 happy

--- src/main/python/photolib/mosaic.py
+++ src/main/python/photolib/mosaic.py
@@ -15,8 +15,10 @@ def match_any(filename, patterns):
             return True
     return False
 
+
 def match_any_photo_formats(filename):
     return match_any(filename, ["*.jp*g", "*.JP*G", "*.png", "*.PNG"])
+
 
 class Preparer(object):
     def __init__(self, tiles_dir):
@@ -52,7 +54,11 @@ class Preparer(object):
             if os.path.exists(tables_mxt):
                 os.remove(tables_mxt)
             with open(os.devnull, 'w') as devnull:
-                subprocess.call(["metapixel-prepare", "--width=%i" % TILE_SIZE, "--height=%i" % TILE_SIZE, dirpath, tmpdir], stdout=devnull)
+                subprocess.call(["metapixel-prepare",
+                                 "--width=%i" % TILE_SIZE,
+                                 "--height=%i" % TILE_SIZE,
+                                 dirpath,
+                                 tmpdir], stdout=devnull)
             with open(tables_mxt, "w") as new_tables_mxt:
                 with open(os.path.join(tmpdir, TABLES_MXT)) as tmp_tables_mxt:
                     for line in tmp_tables_mxt:
