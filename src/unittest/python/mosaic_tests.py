0d790dd8a39b60ee1b5e590db2826487fa8e421d
Arne Hilmann <arne@hilmann.de>
2015-06-24 13:41:33 +0200

    add mosaic tests

---
+++ src/unittest/python/mosaic_tests.py
@@ -0,0 +1,27 @@
+import unittest
+from mock import patch, mock_open
+
+import photolib.mosaic as mosaic
+
+
+class MosaicTest(unittest.TestCase):
+
+    def test_match_photos(self):
+        self.assertTrue(mosaic.match_any_photo_formats("foo.png"))
+        self.assertFalse(mosaic.match_any_photo_formats("bar.txt"))
+
+    def test_preparer(self):
+        p = mosaic.Preparer("src/resources/tiles")
+
+        m = mock_open()
+        with patch("tempfile.mkdtemp", return_value="/invalid_dir"):
+            with patch("subprocess.call"):
+                with patch("shutil.rmtree"):
+                    with patch("__builtin__.open", m, create=True):
+                        p.main()
+        m.assert_any_call("src/resources/tiles/tables.mxt", "w")
+        m.assert_any_call("/invalid_dir/tables.mxt")
+
+
+if __name__ == "__main__":
+    unittest.main()
