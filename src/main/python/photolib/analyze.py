4dbe97cd5dc147ae16c8a05af9aa66bf7f2ede3f
Arne Hilmann <arne@hilmann.de>
2015-06-26 15:25:33 +0200

    implement exif-based creating of tiles

--- src/main/python/photolib/analyze.py
+++ src/main/python/photolib/analyze.py
@@ -78,7 +78,7 @@ class Analyzer(object):
             os.makedirs(target_dir)
         filename = os.path.join(target_dir, "%s%s" % (basename, suffix))
         if os.path.exists(filename):
-            logging.info("%s: tile for %s already exists, skipping" % (filepath, name))
+            logging.debug("%s: tile for %s already exists, skipping" % (filepath, name))
             return filename
         logging.info("%s: creating tile for %s" % (filepath, name))
         exit_code = subprocess.check_call(["convert", filepath, "-resize", "240x240^",
@@ -91,7 +91,7 @@ class Analyzer(object):
             os.makedirs(target_dir)
         filename = os.path.join(target_dir, "%s.face%s" % (basename, suffix))
         if os.path.exists(filename):
-            logging.info("%s: face image for %s already exists, skipping" % (filepath, name))
+            logging.debug("%s: face image for %s already exists, skipping" % (filepath, name))
             return filename
 
         with Image(filename=filepath) as img:
@@ -114,57 +114,118 @@ class Analyzer(object):
 
 
 class NewAnalyzer(object):
-    def __init__(self, photos_dirs, faces_dir, tiles_dir):
+    def __init__(self, photos_dirs, faces_dir, tiles_dir, tiles_size=240):
         self.photos_dirs = [photos_dirs]
         self.faces_dir = faces_dir
         self.tiles_dir = tiles_dir
+        self.tiles_size = tiles_size
 
     def main(self):
         for images_dir in self.photos_dirs:
             logging.info("scanning %s" % images_dir)
             for dirpath, dirnames, filenames in os.walk(images_dir):
                 dirnames.sort()
-                rawexifs = subprocess.check_output(["exiftool", "-j", "-q", os.path.join(images_dir, dirpath)])
+                rawexifs = subprocess.check_output(["exiftool", "-j", "-q", os.path.join(dirpath)])
                 if len(rawexifs) <= 0:
                     continue
                 exifs = json.loads(rawexifs)
                 for exif in exifs:
-                    if "RegionAppliedToDimensionsUnit" not in exif:
-                        continue
-                    region_unit = exif["RegionAppliedToDimensionsUnit"]
-                    if region_unit != "pixel":
-                        logging.warn("unknown RegionAppliedToDimensionsUnit '%s'" % region_unit)
-                        continue
-                    width = exif["RegionAppliedToDimensionsW"]
-                    height = exif["RegionAppliedToDimensionsH"]
+                    self.analyze_exif(exif)
 
-                    if "RegionType" not in exif:
-                        continue
-                    for index, regiontype in enumerate(exif["RegionType"]):
-                        if regiontype != "Face":
-                            continue
-                        area_unit = exif["RegionAreaUnit"][index]
-                        if area_unit != "normalized":
-                            logging.warn("unknown RegionAreaUnit '%s' found" % area_unit)
-                            continue
-                        name = exif["RegionName"][index]
-                        logging.info("face of '%s' found in %s" % (name, exif["SourceFile"]))
+    def analyze_exif(self, exif):
+        if "RegionAppliedToDimensionsUnit" not in exif:
+            return
+        filename = exif["SourceFile"]
+        basename = os.path.basename(filename)
+        region_unit = exif["RegionAppliedToDimensionsUnit"]
+        if region_unit != "pixel":
+            logging.warn("unknown RegionAppliedToDimensionsUnit '%s' found in %s" % (region_unit, basename))
+            return
+        width = exif["RegionAppliedToDimensionsW"]
+        height = exif["RegionAppliedToDimensionsH"]
 
-                        nx = exif["RegionAreaX"][index]
-                        ny = exif["RegionAreaY"][index]
-                        nw = exif["RegionAreaW"][index]
-                        nh = exif["RegionAreaH"][index]
+        if "RegionType" not in exif:
+            return
+        for index, regiontype in enumerate(exif["RegionType"]):
+            if regiontype != "Face":
+                continue
+            area_unit = exif["RegionAreaUnit"][index]
+            if area_unit != "normalized":
+                logging.warn("unknown RegionAreaUnit '%s' found in %s" % (area_unit, basename))
+                continue
+            name = exif["RegionName"][index]
 
-                        x0 = int(nx * width)
-                        y0 = int(ny * height)
-                        x1 = int((nx + nw) * width)
-                        y1 = int((ny + nh) * height)
+            self.create_tile(filename, name, "photos")
 
-                        logging.info("(%s/%s) - (%s/%s)" % (x0, y0, x1, y1))
+            nx = exif["RegionAreaX"][index]
+            ny = exif["RegionAreaY"][index]
+            nw = exif["RegionAreaW"][index]
+            nh = exif["RegionAreaH"][index]
 
-                    for key, value in exif.items():
-                        if key.startswith("Region"):
-                            logging.info("%s: %s" % (key, value))
+            left = int((nx - nw / 2) * width)
+            top = int((ny - nh / 2) * height)
+            right = int((nx + nw / 2) * width)
+            bottom = int((ny + nh / 2) * height)
+
+            # coords = (left, bottom, right, top)
+            coords = (left, top, right, bottom)
+
+            face_filename = self.create_face_image(filename, name, coords)
+            self.create_tile(face_filename, name, "faces")
+        self.dump_exif(exif)
+
+    def create_face_image(self, source_filename, name, coords):
+        target_dir = os.path.join(self.faces_dir, name)
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir)
+
+        basename, suffix = os.path.splitext(os.path.basename(source_filename))
+        target_filename = os.path.join(target_dir, "%s.face.%s%s" % (basename, name, suffix))
+        if os.path.exists(target_filename):
+            logging.debug("%s: face image for %s already exists, skipping" % (target_filename, name))
+            return target_filename
+        logging.info("face of '%s' found in %s, at %s" % (name, basename, "%s/%s %s/%s" % coords))
+
+        # cmd = ["convert", source_filename,
+        #        "-fill", "none",
+        #        "-stroke", "yellow",
+        #        "-strokewidth", "20",
+        #        "-draw", "arc %i,%i %i,%i 0,360" % coords,
+        #        target_filename]
+
+        # crop_string = "%ix%i+%i+%i" % (right - left, bottom - top, left, top)
+        crop_string = "%ix%i+%i+%i" % (coords[2] - coords[0], coords[3] - coords[1], coords[0], coords[1])
+        cmd = ["convert", source_filename, "-crop", crop_string, "+repage", target_filename]
+        logging.info(" ".join(cmd))
+        exit_code = subprocess.check_call(cmd)
+        logging.debug("exit code: %i" % exit_code)
+        return target_filename
+
+    def create_tile(self, source_filename, name, subfolder):
+        suffix = ".png"
+        basename, suffix = os.path.splitext(os.path.basename(source_filename))
+        target_dir = os.path.join(self.tiles_dir, subfolder, name)
+        target_filename = os.path.join(target_dir, basename + suffix)
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir)
+        if os.path.exists(target_filename):
+            logging.debug("%s: tile for %s already exists, skipping" % (target_filename, name))
+            return target_filename
+        logging.info("%s: creating tile for %s" % (target_filename, name))
+        exit_code = subprocess.check_call(["convert", source_filename,
+                                           "-resize", "%sx%s^" % (self.tiles_size, self.tiles_size),
+                                           "-gravity", "Center",
+                                           "-crop", "%sx%s+0+0!" % (self.tiles_size, self.tiles_size),
+                                           target_filename])
+        logging.debug("exit code: %i" % exit_code)
+        return target_filename
+
+
+    @staticmethod
+    def dump_exif(exif):
+        for key, value in exif.items():
+            if key.startswith("Region"):
+                logging.debug("exif  %s: %s" % (key, value))
 
 
 if __name__ == "__main__":
