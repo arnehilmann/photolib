2ed1165532e7138bf8034e75371a794bdaff9504
Arne Hilmann <arne@hilmann.de>
2015-07-02 12:53:32 +0200

    remove datetime-based tests

--- src/unittest/python/photoimporter_tests.py
+++ src/unittest/python/photoimporter_tests.py
@@ -1,7 +1,6 @@
 import unittest
-from mock import MagicMock, patch
+from mock import patch
 
-import os
 from datetime import datetime
 import logging
 
@@ -16,12 +15,6 @@ class PhotoImporterTest(unittest.TestCase):
 
     def test_init(self):
         self.assertEqual(self.pi.number_prefix, "me")
-
-    # def test_guess_create_date_using_ctime(self):
-    #     os.path.getctime = MagicMock()
-    #     os.path.getctime.returnvalue = datetime.now()
-
-    #     self.assertEqual(self.pi.guess_create_date_using_ctime("foo", "bar"), datetime(1970, 1, 1, 1, 0, 1))
 
     def test_guess_create_date_using_path(self):
         self.assertEqual(self.pi.guess_create_date_using_path("1970-02-02"), datetime(1970, 2, 2, 0, 0))
